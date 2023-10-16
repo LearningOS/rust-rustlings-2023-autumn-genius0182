4e9d9da1edf781605bc251d437f2af5a7203f895
hao.lu@sbibits.com <hao.lu@sbibits.com>
2023-10-16 17:51:18 +0800

    6 workday

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,32 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let mut x: Person = Person::default();
+        let mut default: Person = Person::default();
+
+        if s == "" {
+            return default;
+        }
+
+        let v: Vec<&str> = s.split(",").collect();
+
+        if v.len() != 2 {
+            return default;
+        }
+
+        if (&v[0]).to_string() != "" {
+            x.name = (&v[0]).to_string();
+        } else {
+            return default;
+        }
+
+        match v[1].parse::<usize>() {
+            Ok(age) => x.age = age,
+            Err(e) => return default,
+        }
+        x
     }
 }
 
